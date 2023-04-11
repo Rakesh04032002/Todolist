ae8dceddacc824699ef5c9bb1e09bb5bd4235f8d
Rakesh04032002 <100091606+Rakesh04032002@users.noreply.github.com>
2023-04-11 11:34:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const item3=new Item({
 const defaultitems=[item1,item2,item3];
 
 app.get("/",function(req,res){
-    Item.find({}).then(function(foundItems){
+    
+    Item.find({}).estimatedDocumentCount().exec().then(function(foundItems){
         //using this if statement we ensure that when the array founditems is empty then insert our defaultitetms array to the database and using this whenever we run our server the items will not inserted inthe array again and again
         if(foundItems.length===0){
             Item.insertMany(defaultitems);
@@ -49,7 +50,7 @@ app.get("/",function(req,res){
 
 app.get("/:customlistname",function(req,res){
     const customlistname=_.capitalize(req.params.customlistname);
-    llist.findOne({name:customlistname}).then(function(foundlist){
+    llist.findOne({name:customlistname}).estimatedDocumentCount().exec().then(function(foundlist){
         if(!foundlist){
                 // console.log("doesn't exits");
                 //creating new list
@@ -84,7 +85,7 @@ app.post("/",function(req,res){
         newentity.save();
         res.redirect("/");
     }else{
-        llist.findOne({name:listname}).then(function(foundlists){
+        llist.findOne({name:listname}).estimatedDocumentCount().exec().then(function(foundlists){
             foundlists.items.push(newentity);
             foundlists.save();
             // console.log(foundlists);
@@ -100,7 +101,7 @@ app.post("/delete",function(req,res){
     const checkeditemid=req.body.check;
     const listName=req.body.listName;
     if(listName==="TODAY's"){
-        Item.findByIdAndRemove(checkeditemid).then(function(err){
+        Item.findByIdAndRemove(checkeditemid).estimatedDocumentCount().exec().then(function(err){
             // if(!err){
             //     console.log("successfully removed");
             // }
@@ -110,7 +111,7 @@ app.post("/delete",function(req,res){
         //it will find the item from the listname and from the items array of listschema it will pull the id which is matched the item of items array and update that
         let doc =  llist.findOneAndUpdate({name:listName}, {$pull: {items: {_id: checkeditemid}}}, {
             new: true
-            }).then(function (foundList)
+            }).estimatedDocumentCount().exec().then(function (foundList)
             {
                 res.redirect("/" + listName);
             }).catch( err => console.log(err));
